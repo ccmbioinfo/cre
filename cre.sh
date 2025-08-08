e71f9dc6459b665c9349aef0cf5baf8aee1bb59c
Anjali Jain <anjali.jain2299@gmail.com>
2025-08-08 12:15:34 -0400

    changed the Rscript that's submitted to generate report to cre.vcf2db.R

--- cre.sh
+++ cre.sh
@@ -247,7 +247,7 @@ function f_make_report
 
     echo GENERATING REPORT WITH TYPE: "${type}"
 
-    Rscript $cre/nonC4R_report_generation.r $family "${type}" "${database}"
+    Rscript $cre/cre.vcf2db.R $family "${type}" "${database}"
 
     
     cd $family
